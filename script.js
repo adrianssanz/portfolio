a2fd937221bd47626c3e76f9abb409cf91973dec
adrianssanz <sanzadrian@hotmail.es>
2024-12-13 18:26:59 +0000

    Añadir boton volver arriba

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 document.getElementById('ver-mas').addEventListener('click', function(e) {
-    e.preventDefault();
-    
-    var oculto = document.getElementById('vermas');
+  e.preventDefault();
   
-   if (oculto.classList.contains('ocultar')) {
+  var oculto = document.getElementById('vermas');
+
+  if (oculto.classList.contains('ocultar')) {
       oculto.classList.remove('ocultar');
       oculto.classList.add('mostrar');
       this.textContent = "Ver menos";
-    } else {
+  } else {
       oculto.classList.remove('mostrar');
       oculto.classList.add('ocultar');
       this.textContent = "Ver más";
-    }
-  });
-  
-  //Cambio de prueba
+  }
+});
+
+const botonFlecha = document.getElementById('volverArriba');
+
+window.onscroll = function() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      botonFlecha.classList.add('show');
+  } else {
+      botonFlecha.classList.remove('show');
+  }
+};
+
+botonFlecha.onclick = function() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+};
