fec7e4d3255331636629f99ee970c401aa0682e7
Adrian Sanz <sanzadrian@hotmail.es>
2025-01-22 20:06:45 +0100

    Renombrado componente header a estructura y creado nuevo componente EstructuraComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import
  { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
+import { EstructuraComponent } from './layout/estructura/estructura.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true, // Make sure 'standalone: true' is present
-  imports: [HeaderComponent] // Import HeaderComponent here
+  imports: [EstructuraComponent] // Import HeaderComponent here
 })
 export class AppComponent {
   title = 'my-app';
---
+++ src/app/layout/estructura/estructura.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+
+@Component({
+  selector: 'app-estructura',
+  imports: [HeaderComponent],
+  templateUrl: './estructura.component.html',
+  styleUrl: './estructura.component.css'
+})
+export class EstructuraComponent {
+
+}
