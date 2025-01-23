62967fc4e13d66ccae518c753071740ba8bb8a5f
Adrian Sanz <sanzadrian@hotmail.es>
2025-01-23 17:13:55 +0100

    Añadido componente 'Educacion' al componente 'Estructura' y actualizado la lista de imports.

--- src/app/layout/estructura/estructura.component.ts
+++ src/app/layout/estructura/estructura.component.ts
@@ -3,10 +3,17 @@ import { HeaderComponent } from '../../components/header/header.component';
 import { SobreMiComponent } from '../../components/sobre-mi/sobre-mi.component';
 import { TecnologiasComponent } from '../../components/tecnologias/tecnologias.component';
 import { ExperienciaComponent } from '../../components/experiencia/experiencia.component';
+import { EducacionComponent } from '../../components/educacion/educacion.component';
 
 @Component({
   selector: 'app-estructura',
-  imports: [HeaderComponent, SobreMiComponent, TecnologiasComponent, ExperienciaComponent],
+  imports: [
+    HeaderComponent,
+    SobreMiComponent,
+    TecnologiasComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+  ],
   templateUrl: './estructura.component.html',
   styleUrl: './estructura.component.css',
 })
