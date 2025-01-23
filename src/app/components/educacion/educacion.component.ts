980b1b975ef3104877fa7babeaf8acf41f3e4c38
Adrian Sanz <sanzadrian@hotmail.es>
2025-01-23 17:13:47 +0100

    Añadido componente 'Educacion' con su plantilla, estilos, pruebas unitarias y un servicio para obtener datos de educación desde un archivo JSON.

---
+++ src/app/components/educacion/educacion.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DataServiceService } from '../../services/data-service.service';
+import { Educacion } from '../../interfaces/interfaces';
+import { FormatDescripcionPipe } from '../../pipes/format-descripcion.pipe';
+
+@Component({
+  selector: 'app-educacion',
+  imports: [CommonModule, FormatDescripcionPipe],
+  templateUrl: './educacion.component.html',
+  styleUrl: './educacion.component.css'
+})
+export class EducacionComponent implements OnInit {
+  constructor(private dataService: DataServiceService){}
+  
+  educaciones!: Educacion[];
+
+  ngOnInit(): void {
+    this.getEducacion();
+  }
+
+  getEducacion(): void {
+    this.dataService.getEducacion().subscribe((respuesta) => {
+      this.educaciones = respuesta.educacion;
+      console.log(this.educaciones);
+    });
+  }
+
+}
