003d1636a84c87b52413960dfac258f782a80571
Adrian Sanz <sanzadrian@hotmail.es>
2025-01-24 16:06:28 +0100

    Añadido efecto de resplandor al cursor mediante estilos CSS y actualizado el componente para seguir su posición en la pantalla.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import
- { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { EstructuraComponent } from './layout/estructura/estructura.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true, // Make sure 'standalone: true' is present
-  imports: [EstructuraComponent] // Import HeaderComponent here
+  imports: [EstructuraComponent,CommonModule], // Import HeaderComponent here
 })
 export class AppComponent {
   title = 'my-app';
+
+  x = 0;
+  y = 0;
+
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    this.x = event.clientX;
+    this.y = event.clientY;
+  }
 }
