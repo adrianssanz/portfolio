9c7ef18ca5ae1f433f88a785bc4e18314dd433a4
Adrian Sanz <sanzadrian@hotmail.es>
2025-01-26 12:09:51 +0100

    Implementada la sección de proyectos: se añadió la lógica para obtener y mostrar proyectos desde el servicio, se mejoró el diseño y se optimizó el HTML para una mejor presentación.

--- src/app/layout/layout-proyectos/layout-proyectos.component.ts
+++ src/app/layout/layout-proyectos/layout-proyectos.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { CommonModule } from '@angular/common';
+import { DataServiceService } from '../../services/data-service.service';
+import { Proyecto } from '../../interfaces/interfaces';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-layout-proyectos',
-  imports: [],
+  imports: [CommonModule, FooterComponent, RouterModule],
   templateUrl: './layout-proyectos.component.html',
-  styleUrl: './layout-proyectos.component.css'
+  styleUrl: './layout-proyectos.component.css',
 })
-export class LayoutProyectosComponent {
+export class LayoutProyectosComponent implements OnInit {
+  constructor(
+    private dataService: DataServiceService
+  ) {}
 
+  proyectos!: Proyecto[];
+
+  ngOnInit(): void {
+    this.getProyectos();
+  }
+
+  getProyectos(): void {
+    this.dataService.getProyectos().subscribe((respuesta) => {
+      this.proyectos = respuesta.proyectos;
+    });
+  }
 }
