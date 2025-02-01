a6dcebe7b6100ea60602dab7b61f5dd03c60ab69
Adrian Sanz <sanzadrian@hotmail.es>
2025-02-01 16:11:18 +0100

    Modificado servicio para no repetir codigo.

--- src/app/services/data-service.service.ts
+++ src/app/services/data-service.service.ts
@@ -6,26 +6,11 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class DataServiceService {
-  private tecnologiasUrl = './assets/data/tecnologias.json';
-  private empresasUrl = './assets/data/empresas.json';
-  private educacionUrl = './assets/data/educacion.json';
-  private proyectosUrl = './assets/data/proyectos.json';
+  private dataUrl = './assets/data/';
 
   constructor(private http: HttpClient) {}
 
-  getTecnologias(): Observable<any> {
-    return this.http.get(this.tecnologiasUrl);
-  }
-
-  getEducacion():Observable<any> {
-    return this.http.get(this.educacionUrl);
-  }
-
-  getProyectos():Observable<any> {
-    return this.http.get(this.proyectosUrl);
-  }
-
-  getEmpresas(): Observable<any>{
-    return this.http.get(this.empresasUrl);
+  getData(file: string): Observable<any>{
+    return this.http.get(this.dataUrl + file);
   }
 }
