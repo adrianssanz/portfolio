84206977b3067d1b53dd7e4d4b7c6e837aed4b9d
Adrian Sanz <sanzadrian@hotmail.es>
2025-02-07 22:44:57 +0100

    Creado servicio para gestionar el idioma de la web

---
+++ src/app/services/language.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LanguageService {
+  private readonly LANG_KEY = 'language';
+
+  constructor() { }
+
+  setLanguage(isEnglish: boolean): void{
+    localStorage.setItem(this.LANG_KEY, JSON.stringify(isEnglish));
+  }
+
+  getLanguage(): boolean{
+    const lang = localStorage.getItem(this.LANG_KEY);
+    return lang ? JSON.parse(lang): true;
+  }
+
+  toggleLanguage(): void{
+    const currentLang = this.getLanguage();
+    this.setLanguage(!currentLang);
+  }
+}
