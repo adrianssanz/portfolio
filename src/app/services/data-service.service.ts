b8228ba5a0dc4cd93614ee2a83ab74fa504d37ae
Adrian Sanz <sanzadrian@hotmail.es>
2025-02-07 22:45:15 +0100

    Modificado servicio data para manejar datos en ingles

--- src/app/services/data-service.service.ts
+++ src/app/services/data-service.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { LanguageService } from './language.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataServiceService {
   private dataUrl = './assets/data/data.json';
+  private dataUrlEn = './assets/data/dataEn.json';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private languageService: LanguageService) {}
 
   getData(): Observable<any>{
+    if (this.languageService.getLanguage()){
+      return this.http.get(this.dataUrlEn);
+    }
     return this.http.get(this.dataUrl);
   }
 }
