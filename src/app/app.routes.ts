6d66431011c7916dc822db393f2ea5c7ef2a7657
Adrian Sanz <sanzadrian@hotmail.es>
2025-02-12 16:33:49 +0100

    Cambiada ruta /proyectos a /projects

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,7 +9,7 @@ export const routes: Routes = [
         component: EstructuraComponent
     },
     {
-        path:'proyectos',
+        path:'projects',
         component: LayoutProyectosComponent
     },
     {
