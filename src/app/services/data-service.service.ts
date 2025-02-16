5d17bd14b94c10cb7ecbfb858d2b4f14b616350a
Adrian Sanz <sanzadrian@hotmail.es>
2025-02-16 12:47:41 +0100

    Modificado servicio para obtener datos de GitHub Gists

--- src/app/services/data-service.service.ts
+++ src/app/services/data-service.service.ts
@@ -1,21 +1,36 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, Observable } from 'rxjs';
 import { LanguageService } from './language.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataServiceService {
-  private dataUrl = './assets/data/data.json';
-  private dataUrlEn = './assets/data/dataEn.json';
+  private dataUrlEs = 'https://gist.githubusercontent.com/adrianssanz/87319ce5325d3486f32ac8e4e0eff4cb/raw';
+  private dataUrlEn = 'https://gist.githubusercontent.com/adrianssanz/cc45a685e56f05aa5ecf44f5ceaffe39/raw';
+
+  private dataEsJson = './assets/data/data.json';
+  private dataEnJson = './assets/data/dataEn.json';
+
+  getData(): Observable<any> {
+    if (this.languageService.getLanguage()) {
+      return this.http.get(this.dataUrlEn).pipe(
+        catchError(() => {
+          // Si hay un error o no devuelve nada, se usa el archivo local en inglés
+          return this.http.get(this.dataEnJson);
+        })
+      );
+    } else {
+      return this.http.get(this.dataUrlEs).pipe(
+        catchError(() => {
+          // Si hay un error o no devuelve nada, se usa el archivo local en español
+          return this.http.get(this.dataEsJson);
+        })
+      );
+    }
+  }
 
   constructor(private http: HttpClient, private languageService: LanguageService) {}
 
-  getData(): Observable<any>{
-    if (this.languageService.getLanguage()){
-      return this.http.get(this.dataUrlEn);
-    }
-    return this.http.get(this.dataUrl);
-  }
 }
